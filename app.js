6c0fd7ddf8d32b5ab7d49df21edb8e26f00e26a6
Asaf Erez <63843803+aceerez@users.noreply.github.com>
2020-12-30 18:56:44 +0200

    change massage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('HI!,this is my first AWS PipLine'));
+app.get('/', (req, res) => res.send('this is my first AWS pipline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
